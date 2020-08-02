4d813dc39f954faff59d5a46c2a5a5780354b396
Sercan Altundas <sercan.altundas@gmail.com>
2020-08-03 01:43:11 +0300

    game states and curtain implemented

--- main.lua
+++ main.lua
@@ -1,22 +1,72 @@
+--source files
 require 'source.settings'
 require 'source.debug'
 require 'source.utils'
+require 'source.states'
+
+--plugins
+_input = require 'plugins.Input'
 
 function love.load()
+    gameState = SceneStates.debug
+    stateToGo = SceneStates.debug
+
+    input = _input()
+    input:bind('space', 'next')
+
     drawInit()
 end
 
-function love.update()
+function love.update(dt)
+    if gameState == SceneStates.menu then 
+        if input:pressed('next') then
+            stateToGo = SceneStates.intro
+            curtainIsOn = true
+        end
+    elseif gameState == SceneStates.intro then
+        if input:pressed('next') then
+            stateToGo = SceneStates.game
+            curtainIsOn = true
+        end
+    elseif gameState == SceneStates.game then
+        if input:pressed('next') then
+            stateToGo = SceneStates.credits
+            curtainIsOn = true
+        end
+    elseif gameState == SceneStates.credits then
+        if input:pressed('next') then
+            stateToGo = SceneStates.debug
+            curtainIsOn = true
+        end
+    elseif gameState == SceneStates.debug then 
+        if input:pressed('next') then
+            stateToGo = SceneStates.menu
+            curtainIsOn = true
+        end
+    end
 
+    updateCurtain(dt)
 end
 
 function love.draw()
     --begin drawing into canvas
     drawBegin()
 
-    --
-    lg.draw(debugImage)
+    --states
+    if gameState == SceneStates.menu then 
 
+    elseif gameState == SceneStates.intro then
+
+    elseif gameState == SceneStates.game then
+
+    elseif gameState == SceneStates.credits then
+
+    end
+
+    --draw debug
+    drawDebug(gameState)
+    --draw curtain
+    drawCurtain()
     --end drawing into canvas
     drawEnd()   
 end
