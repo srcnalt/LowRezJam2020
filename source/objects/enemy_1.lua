fecd3c35d041b22b0abf8a05f66d27710e6c08d0
Sercan Altundas <sercan.altundas@gmail.com>
2020-08-14 14:48:16 +0300

    enemy attack

--- source/objects/enemy_1.lua
+++ source/objects/enemy_1.lua
@@ -1,41 +1,42 @@
 local EnemyOne = {}
 EnemyOne.__index = EnemyOne
 
-local spawned = false
-
 function EnemyOne.new()
-  local enemy = lg.newImage('graphics/enemies/enemy_1.png')
-  local animEnemy = anim.new(enemy, 64, 64, 0.2)
-  
-  input:bind('a', 'spawn_enemy')
-  input:bind('b', 'reset_enemy')
+  local idle = lg.newImage('graphics/enemies/Enemy_Skeleton_Idle.png')
+  local animIdle = anim.new(idle, 64, 64, 0.2)
 
+  local attack = lg.newImage('graphics/enemies/Enemy_Skeleton_attack.png')
+  local animAttack = anim.new(attack, 64, 64, 0.2)
+  
   return setmetatable({
-    anim = animEnemy,
+    anim = {
+      idle = animIdle,
+      attack = animAttack
+    },
+    attack = {
+      delay = 2,
+      time = 0,
+      length = 1
+    },
     time = 0,
     posX = 0,
     posY = 0,
     scale = 0.1,
     linearPos = 1.5,
-    encountered = false,
-    visible = false
+    state = EnemyState.asleep,
+    encountered = false
   }, EnemyOne)
 end
 
 function EnemyOne:update(dt)
-  self.visible = self.linearPos - globalPos < 1
+  CheckState(self)
 
-  if self.visible then
-    self.scale = 1 - (self.linearPos - globalPos)
+  if self.state == EnemyState.asleep then
+    --
+  elseif self.state == EnemyState.idle then
+    ScaleBody(self, dt)
 
-    if self.scale > 1 then self.scale = 1 end
-
-    self.time = self.time + dt * 4
-
-    self.posX = math.sin(self.time) * 2 + 32 - 32 * self.scale
-    self.posY = math.cos(self.time) * 2 + 32 - 32 * self.scale
-
-    if not encountered and self.scale > 0.3 then
+    if not encountered and self.scale > 0.4 then
       encountered = true
     end
 
@@ -47,16 +48,54 @@ function EnemyOne:update(dt)
       self.linearPos = self.linearPos - dt / 4
     end
 
-    self.anim:update(dt)
+    self.anim.idle:update(dt)
+  elseif self.state == EnemyState.attack then
+    ScaleBody(self, dt)
+    self.attack.time = self.attack.time + dt
+
+    if self.attack.time >= self.attack.delay + self.attack.length then
+      self.attack.time = 0
+    elseif self.attack.time >= self.attack.delay then
+      self.anim.attack:update(dt)
+    else
+      self.anim.idle:update(dt)
+    end    
   end
 end
 
 function EnemyOne:draw()
-  if self.visible then
+  if self.state == EnemyState.idle then
     lg.setColor( 255, 255, 255, self.scale)
-    self.anim:draw(self.posX, self.posY, 0, self.scale, self.scale)
+    self.anim.idle:draw(self.posX, self.posY, 0, self.scale, self.scale)
     lg.setColor( 255, 255, 255, 1 )  
+   elseif self.state == EnemyState.attack then
+    if self.attack.time >= self.attack.delay then
+      self.anim.attack:draw(self.posX, self.posY, 0, self.scale, self.scale)
+    else
+      self.anim.idle:draw(self.posX, self.posY, 0, self.scale, self.scale)
+    end 
   end
+end
+
+function CheckState(self)
+  if self.linearPos - globalPos > 1 then
+    self.state = EnemyState.asleep
+  elseif self.linearPos - globalPos > 0 then
+    self.state = EnemyState.idle
+  else
+    self.state = EnemyState.attack
+  end 
+end
+
+function ScaleBody(self, dt)
+  self.scale = 1 - (self.linearPos - globalPos)
+
+  if self.scale > 1 then self.scale = 1 end
+
+  self.time = self.time + dt * 4
+
+  self.posX = math.sin(self.time) * 2 + 32 - 32 * self.scale
+  self.posY = math.cos(self.time) * 2 + 32 - 32 * self.scale
 end
 
 return setmetatable({}, {__call = function(_, ...) return EnemyOne.new(...) end})
