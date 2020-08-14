6170cbead3c6bca244495c66c2671de024f0d80c
Sercan Altundas <sercan.altundas@gmail.com>
2020-08-14 18:09:49 +0300

    Update map.lua

--- source/objects/map.lua
+++ source/objects/map.lua
@@ -2,16 +2,30 @@ local Map = {}
 Map.__index = Map
 
 local moving = MovementStates.stop
+local currentAnim = {}
 
 function Map.new()
-    local mapStraight = lg.newImage('graphics/map/64x64Scene.png')
-    local animMapStraight = anim.new(mapStraight, 64, 64, 0)
+    local mapForward = lg.newImage('graphics/map/64x64Scene.png')
+    local animForward = anim.new(mapForward, 64, 64, 0)
+
+    local mapLeft = lg.newImage('graphics/map/Road2Left.png')
+    local animLeft = anim.new(mapLeft, 64, 64, 0)
+    
+    local mapRight = lg.newImage('graphics/map/Road2Right.png')
+    local animRight = anim.new(mapRight, 64, 64, 0)
 
     input:bind('up', 'move_forward')
     input:bind('down', 'move_backwards')
 
+    currentAnim = animForward
+
     return setmetatable({
-        anim = animMapStraight,
+        anim = {
+            forward = animForward,
+            left    = animLeft,
+            right   = animRight
+        },
+        time = 0,
         moving = moving
     }, Map)
 end
@@ -20,6 +34,7 @@ function Map:update(dt)
     if input:down('move_forward') then 
         moving = MovementStates.forward
         globalPos = globalPos + dt / 2
+        RoadSwitch(self)
     elseif input:released('move_forward') then
         moving = MovementStates.stop
     end
@@ -27,23 +42,51 @@ function Map:update(dt)
     if input:down('move_backwards') then 
         moving = MovementStates.backwards
         globalPos = globalPos - dt / 2
+        RoadSwitchBack(self)
     elseif input:released('move_backwards') then
         moving = MovementStates.stop
     end
     
-    self.anim:update(dt)
+    currentAnim:update(dt)
 end
 
 function Map:draw()
     if moving == MovementStates.forward then
-        self.anim.speed = 0.1
+        currentAnim.speed = 0.1
     elseif moving == MovementStates.backwards then
-        self.anim.speed = -0.1
+        currentAnim.speed = -0.1
     else
-        self.anim.speed = 0
+        currentAnim.speed = 0
     end
 
-    self.anim:draw()
+    currentAnim:draw()
+end
+
+function RoadSwitch(self)
+    if currentAnim == self.anim.forward and self.anim.forward.pos == #self.anim.forward.frames then
+        if math.random (4) == 4 then
+            currentAnim = self.anim.left
+        elseif math.random (4) == 3 then
+            currentAnim = self.anim.right
+        end
+        currentAnim:reset()
+    elseif currentAnim == self.anim.left and self.anim.left.pos == #self.anim.left.frames then
+        currentAnim = self.anim.forward
+        currentAnim:reset()
+    elseif currentAnim == self.anim.right and self.anim.right.pos == #self.anim.right.frames then
+        currentAnim = self.anim.forward
+        currentAnim:reset()
+    end
+end
+
+function RoadSwitchBack(self)
+    if currentAnim == self.anim.left and self.anim.left.pos == 1 then
+        currentAnim = self.anim.forward
+        currentAnim:reset()
+    elseif currentAnim == self.anim.right and self.anim.right.pos == 1 then
+        currentAnim = self.anim.forward
+        currentAnim:reset()
+    end
 end
 
 return setmetatable({}, {__call = function(_, ...) return Map.new(...) end})
