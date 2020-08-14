1a5de544b91e82c15e79b073bec3c91fa699c10d
Sercan Altundas <sercan.altundas@gmail.com>
2020-08-14 22:04:30 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ _player = require 'source.objects.player'
 _input = require 'plugins.Input'
 
 function love.load()
-    gameState = SceneStates.menu
+    gameState = SceneStates.game
     stateToGo = SceneStates.debug
 
     globalPos = 0
@@ -31,60 +31,52 @@ end
 
 function love.update(dt)
     if gameState == SceneStates.menu then 
-        if input:pressed('next') then
-            stateToGo = SceneStates.intro
-            curtainIsOn = true
-        end
-        
-        map:update(dt)
-        enemyOne:update(dt)
-        player:update(dt)
+      CheckNextStateToGo(SceneStates.menu)
     elseif gameState == SceneStates.intro then
-        if input:pressed('next') then
-            stateToGo = SceneStates.game
-            curtainIsOn = true
-        end
+      CheckNextStateToGo(SceneStates.intro)
     elseif gameState == SceneStates.game then
-        if input:pressed('next') then
-            stateToGo = SceneStates.credits
-            curtainIsOn = true
-        end
+      CheckNextStateToGo(SceneStates.game)
+
+      map:update(dt)
+      enemyOne:update(dt)
+      player:update(dt)
     elseif gameState == SceneStates.credits then
-        if input:pressed('next') then
-            stateToGo = SceneStates.debug
-            curtainIsOn = true
-        end
+      CheckNextStateToGo(SceneStates.credits)
     elseif gameState == SceneStates.debug then 
-        if input:pressed('next') then
-            stateToGo = SceneStates.menu
-            curtainIsOn = true
-        end
+      CheckNextStateToGo(SceneStates.debug)
     end
 
     updateCurtain(dt)
 end
 
 function love.draw()
-    --begin drawing into canvas
-    drawBegin()
+  --begin drawing into canvas
+  drawBegin()
 
-    --states
-    if gameState == SceneStates.menu then 
-        map:draw()
-        enemyOne:draw()
-        player:draw()
-    elseif gameState == SceneStates.intro then
+  --states
+  if gameState == SceneStates.menu then 
 
-    elseif gameState == SceneStates.game then
+  elseif gameState == SceneStates.intro then
 
-    elseif gameState == SceneStates.credits then
+  elseif gameState == SceneStates.game then
+    map:draw()
+    enemyOne:draw()
+    player:draw()
+  elseif gameState == SceneStates.credits then
 
-    end
+  end
 
-    --draw debug
-    drawDebug(gameState)
-    --draw curtain
-    drawCurtain()
-    --end drawing into canvas
-    drawEnd()   
+  --draw debug
+  drawDebug(gameState)
+  --draw curtain
+  drawCurtain()
+  --end drawing into canvas
+  drawEnd()   
+end
+
+function CheckNextStateToGo(state)
+  if input:pressed('next') then
+    stateToGo = state
+    curtainIsOn = true
+  end
 end
